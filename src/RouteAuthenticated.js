4381cf10a585eb51b8fb975aa109bab330062a3a
Johan Tötterman <johan.totterman@sprintit.fi>
2019-02-16 23:36:13 +0200

    Make routing simpler and move the authenticated route to the src/-dir.

---
+++ src/RouteAuthenticated.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { UserContext } from './UserContext'
+
+const RouteAuthenticated = props => {
+  const userContext = useContext(UserContext)
+  const isAuthenticated = userContext.isAuthenticated
+  const { component: Component, ...rest } = props
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        return isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login'
+            }}
+          />
+        )
+      }}
+    />
+  )
+}
+
+export default RouteAuthenticated
