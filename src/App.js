e1aca0e563476b40aec426fa2e9cc0bc96357114
Johan Tötterman <johan.totterman@sprintit.fi>
2019-02-16 23:39:29 +0200

    Rewritten using React Hooks.

--- src/App.js
+++ src/App.js
@@ -1,125 +1,77 @@
-import React, { Component, Fragment } from 'react'
+import React, { useState, useEffect, useContext, Fragment } from 'react'
 import { NavLink, Link, withRouter } from 'react-router-dom'
 import { Nav, Navbar } from 'react-bootstrap'
+import { UserContext } from './UserContext'
 import Routes from './Routes'
 import './App.css'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+const App = props => {
+  const userContext = useContext(UserContext)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
 
-    this.state = {
-      isAuthenticated: false,
-      isAuthenticating: true
-    }
+  const authenticated = async () => {
+    const auth = await userContext.isAuthenticated
+    setIsAuthenticated(auth)
   }
 
-  async amiLoggedin() {
-    try {
-      const response = await fetch('/api/login')
-      const responseStatus = await response.status
+  useEffect(() => {
+    authenticated()
+  }, [userContext.isAuthenticated])
 
-      if (responseStatus === 200) {
-        this.userHasAuthenticated(true)
-      }
-    } catch (e) {
-      if (e !== 'No current user') {
-        alert(e)
-      }
-    }
-  }
-
-  async logout() {
-    try {
-      const response = await fetch('/api/logout')
-      const responseStatus = await response.status
-
-      if (responseStatus === 200) {
-        this.userHasAuthenticated(true)
-      }
-    } catch (e) {
-      if (e !== 'No current user') {
-        alert(e)
-      }
-    }
-  }
-
-  async componentDidMount() {
-    await this.amiLoggedin()
-    this.setState({ isAuthenticating: false })
-  }
-
-  userHasAuthenticated = authenticated => {
-    this.setState({ isAuthenticated: authenticated })
-  }
-
-  handleLogout = async event => {
-    await this.logout()
-    this.userHasAuthenticated(false)
-    this.props.history.push('/login')
-  }
-
-  render() {
-    const childProps = {
-      isAuthenticated: this.state.isAuthenticated,
-      userHasAuthenticated: this.userHasAuthenticated
-    }
-
-    return (
-      !this.state.isAuthenticating && (
-        <div className="App container">
-          <Navbar bg="light">
-            <Navbar.Brand>
-              <Link to="/" className="nav-link">
-                Stiff Chat Service
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Nav variant="pills">
-                {this.state.isAuthenticated ? (
-                  <Fragment>
-                    <Nav.Item>
-                      <NavLink to="/chat" className="nav-link">
-                        Chat
-                      </NavLink>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <NavLink to="/rooms" className="nav-link">
-                        Rooms
-                      </NavLink>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <NavLink to="/users" className="nav-link">
-                        Users
-                      </NavLink>
-                    </Nav.Item>
-                    <Nav.Item onClick={this.handleLogout} className="nav-link">
-                      Logout
-                    </Nav.Item>
-                  </Fragment>
-                ) : (
-                  <Fragment>
-                    <Nav.Item>
-                      <NavLink to="/signup" className="nav-link">
-                        Signup
-                      </NavLink>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <NavLink to="/login" className="nav-link">
-                        Login
-                      </NavLink>
-                    </Nav.Item>
-                  </Fragment>
-                )}
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <Routes childProps={childProps} />
-        </div>
-      )
-    )
-  }
+  return (
+    <div className="App container">
+      <Navbar bg="light">
+        <Navbar.Brand>
+          <Link to="/" className="nav-link">
+            Stiff Chat Service
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav variant="pills">
+            {isAuthenticated ? (
+              <Fragment>
+                <Nav.Item>
+                  <NavLink to="/chat" className="nav-link">
+                    Chat
+                  </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                  <NavLink to="/rooms" className="nav-link">
+                    Rooms
+                  </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                  <NavLink to="/users" className="nav-link">
+                    Users
+                  </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                  <NavLink to="/logout" className="nav-link">
+                    Logout
+                  </NavLink>
+                </Nav.Item>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <Nav.Item>
+                  <NavLink to="/signup" className="nav-link">
+                    Signup
+                  </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                  <NavLink to="/login" className="nav-link">
+                    Login
+                  </NavLink>
+                </Nav.Item>
+              </Fragment>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
+    </div>
+  )
 }
 
 export default withRouter(App)
