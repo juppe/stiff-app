9fc261229bda855d8a7706a1553a2dd3cbf4d35c
Johan Tötterman <johan.totterman@sprintit.fi>
2019-02-18 21:52:28 +0200

    Moved authentication check to App.js

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,19 +1,6 @@
 import { createContext } from 'react'
 
-const isLoggedIn = async () => {
-  try {
-    const response = await fetch('/api/login')
-    const responseStatus = await response.status
-
-    if (responseStatus === 200) {
-      return true
-    }
-  } catch (e) {
-    if (e !== 'No current user') {
-      alert(e)
-    }
-  }
-  return false
-}
-
-export const UserContext = createContext({ isAuthenticated: isLoggedIn() })
+export const UserContext = createContext({
+  isAuthenticated: false,
+  socket: null
+})
